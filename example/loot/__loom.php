5e9a0030476a3fcb94421d7db859a00824e203ce
doruk eray <doruk@dorkodu.com>
2022-06-23 22:43:32 +0300

    update .gitignore and autoloading for example

--- example/loot/__loom.php
+++ example/loot/__loom.php
@@ -3,15 +3,15 @@
 require_once "loom/Psr4Autoloader.php";
 
 $universalNamespaces = array(
-  'Sage' => './../../source',
+  'Sage'      => './../../source',
   'Sage\Test' => './',
-  'Dorkodu' => 'loot/Dorkodu',
-  'Blog' => 'loot/Blog'
+  'Dorkodu'   => 'loot/Dorkodu',
+  'Blog'      => 'loot/Blog'
 );
 
 $psr4Autoloader = new Psr4Autoloader();
 $psr4Autoloader->register();
 
 foreach ($universalNamespaces as $namespace => $path) {
-  $psr4Autoloader->addNamespace($namespace, $path);
+    $psr4Autoloader->addNamespace($namespace, $path);
 }
