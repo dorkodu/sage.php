5e0be76078e2e65b46d1b6570995920a88972fb7
doruk eray <doruk@dorkodu.com>
2022-07-03 12:53:01 +0300

    update test.php

--- tests/test.php
+++ tests/test.php
@@ -7,17 +7,11 @@
        "arg": {
           "id": 123456
        },
-       "lnk": {
-         "organization": "doruk:organization"
-       }
      },
-     "doruk:organization": {
+     "doruk.organization": {
        "atr": ["name", "website"],
-       "lnk": {
-         "repos": "doruk:organization:repos"
-       }
      },
-     "doruk:organization:repos": {
+     "doruk.organization.repos": {
        "atr": ["title", "description", "starCount"],
        "arg": {
           "filter": "A-Z",
@@ -47,72 +41,3 @@
       }
     }
   GRAPHQL;
-
-  class Stopwatch
-  {
-      protected $beginTimestamp;
-      protected $endTimestamp;
-      protected $usingMicroseconds;
-
-      public function __construct($useMicroseconds = false)
-      {
-          $this->usingMicroseconds = $useMicroseconds;
-      }
-
-      public function start()
-      {
-          if ($this->usingMicroseconds === true) {
-              $this->beginTimestamp = microtime(true);
-          } else {
-              $this->beginTimestamp = time();
-          }
-      }
-
-      public function stop()
-      {
-          if ($this->usingMicroseconds === true) {
-              $this->endTimestamp = microtime(true);
-          } else {
-              $this->endTimestamp = time();
-          }
-      }
-
-      public function reset()
-      {
-          $this->beginTimestamp = 0;
-          $this->endTimestamp = 0;
-      }
-
-      public function isStopped()
-      {
-          return (!empty($this->beginTimestamp) && is_numeric($this->beginTimestamp) && !empty($this->endTimestamp) && is_numeric($this->endTimestamp));
-      }
-
-      public function isRunning()
-      {
-          return (!empty($this->beginTimestamp) && is_numeric($this->beginTimestamp));
-      }
-
-      public function passedTime()
-      {
-          if ($this->isStopped()) {
-              return $this->endTimestamp - $this->beginTimestamp;
-          } elseif ($this->isRunning()) {
-              if ($this->usingMicroseconds === true) {
-                  return microtime(true) - $this->beginTimestamp;
-              } else {
-                  return time() - $this->beginTimestamp;
-              }
-          } else {
-              return 0.00;
-          }
-      }
-  }
-
-  $timer = new Stopwatch(true);
-
-  $timer->start();
-  $parsedJson = json_decode(json: $json, associative: true);
-  $dummy = $parsedJson["doruk"];
-  printf("%.9f", $timer->passedTime());
-  $timer->stop();
